7948e4981d6c7b7bf058d22ccf9225029e0996fa
Lorraine Rojas <ljrojasp@unal.edu.co>
2024-11-28 13:02:22 -0500

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime, timedelta
+import calendar
 
 # Configurar la página
 st.set_page_config(page_title="Dashboard SIKA", layout="wide")
