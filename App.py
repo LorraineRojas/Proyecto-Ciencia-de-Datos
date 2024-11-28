72c49b0ca08f526ed5020300f1de84d1836408bc
Lorraine Rojas <ljrojasp@unal.edu.co>
2024-11-27 20:11:51 -0500

    Actualizar App.py

--- App.py
+++ App.py
@@ -2,42 +2,38 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Cargar el archivo CSV
-@st.cache
-def load_data():
-    return pd.read_csv('bogota.csv')
+# Título y logo
+st.set_page_config(page_title="Dashboard SIKA", layout="wide")
+st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/4/49/Sika_logo.svg", use_column_width=True)
+st.sidebar.title("Subir archivo")
+uploaded_file = st.sidebar.file_uploader("Por favor adjuntar histórico en formato CSV", type=["csv"])
 
-data = load_data()
+if uploaded_file:
+    data = pd.read_csv(uploaded_file)
 
-# Título del Dashboard
-st.title("Dashboard de Forecast por Región y SKU")
+    st.title("Proyección por Región y SKU")
 
-# Selector de Región
-regiones = data['regional_txt'].unique()
-region_seleccionada = st.selectbox("Selecciona una región:", regiones)
+    # Selector de Región
+    regiones = data['regional_txt'].unique()
+    region_seleccionada = st.selectbox("Región", regiones)
 
-# Filtrar los datos por la región seleccionada
-datos_filtrados = data[data['regional_txt'] == region_seleccionada]
+    # Filtrar datos por región
+    datos_filtrados = data[data['regional_txt'] == region_seleccionada]
 
-# Calcular el top 5 de SKU según su forecast
-top_skus = (datos_filtrados.groupby('sku_cd')['forecast_val']
-            .sum()
-            .nlargest(5)
-            .reset_index())
+    # Top 5 de SKU según forecast
+    top_skus = datos_filtrados.groupby('sku_cd')['forecast_val'].sum().nlargest(5).reset_index()
 
-# Gráfica del top 5 SKU
-fig = px.bar(top_skus, x='sku_cd', y='forecast_val', title=f"Top 5 SKU en {region_seleccionada}", labels={'forecast_val': 'Forecast', 'sku_cd': 'SKU'})
-st.plotly_chart(fig)
+    # Gráfica
+    fig = px.bar(top_skus, x='sku_cd', y='forecast_val', title=f"Top 5 de productos vendidos - {region_seleccionada}", labels={'forecast_val': 'Proyección', 'sku_cd': 'Productos x mes'})
+    st.plotly_chart(fig, use_container_width=True)
 
-# Botón para descargar el informe
-@st.cache
-def convertir_csv(df):
-    return df.to_csv(index=False).encode('utf-8')
-
-csv = convertir_csv(datos_filtrados)
-st.download_button(
-    label="Descargar informe completo",
-    data=csv,
-    file_name=f'informe_{region_seleccionada}.csv',
-    mime='text/csv',
-)
+    # Descargar informe
+    csv = datos_filtrados.to_csv(index=False).encode('utf-8')
+    st.download_button(
+        label="DESCARGAR",
+        data=csv,
+        file_name=f'informe_{region_seleccionada}.csv',
+        mime='text/csv',
+    )
+else:
+    st.write("Por favor carga un archivo para visualizar el dashboard.")
